b2006028c609e6ab1850bf068f82ccfd09443716
Louis Young <cool.young0420@gmail.com>
2022-07-31 22:41:54 +0200

    add basic site layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,24 @@
+<div class="container">
+	<div class="row">
+		<slot />
+	</div>
+</div>
+
+<style>
+	:global(body) {
+		margin: 0;
+		background: #eee;
+	}
+	.container {
+		display: flex;
+		justify-content: center;
+	}
+	.row {
+		width: 900px;
+		background: white;
+		padding: 0 1em 1em;
+		margin: 1em 0;
+		border-radius: 10px;
+		box-shadow: 0 0 10px #00000011;
+	}
+</style>
