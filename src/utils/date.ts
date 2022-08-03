53bed2dc3d6898a85fa6822f0a8905123e5772e5
Louis Young <cool.young0420@gmail.com>
2022-08-03 23:36:15 +0200

    utils/date.js: improve type legibility

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,4 +1,6 @@
-export const format = (date: ConstructorParameters<typeof Date>[0]) =>
+type DateArg = ConstructorParameters<typeof Date>[0]
+
+export const format = (date: DateArg) =>
 	new Date(date).toLocaleDateString('en', {
 		day: 'numeric',
 		month: 'long',
