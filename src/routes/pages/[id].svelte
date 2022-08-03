b2104db515afe6090165e7de114849e8cd82da30
Louis Young <cool.young0420@gmail.com>
2022-08-03 23:10:29 +0200

    pages/[id].svelte: format createdAt date

--- src/routes/pages/[id].svelte
+++ src/routes/pages/[id].svelte
@@ -1,15 +1,21 @@
 <script scope="module" lang="ts">
-	import Block from '../../components/Block.svelte'
 	import type { GETBodyReturnTypes } from './[id]'
+
+	import Block from '../../components/Block.svelte'
+	import { format } from '../../utils/date'
 
 	export let blocks: GETBodyReturnTypes['blocks']
 	export let title: GETBodyReturnTypes['title']
 	export let meta: GETBodyReturnTypes['meta']
+
+	const createdAt = meta.createdAt ? format(meta.createdAt) : undefined
 </script>
 
 <h1>
 	<div>{title}</div>
-	<div class="meta">{meta.createdAt}</div>
+	{#if createdAt}
+		<div class="createdAt">{createdAt}</div>
+	{/if}
 </h1>
 
 <div>
@@ -19,7 +25,7 @@
 </div>
 
 <style>
-	.meta {
+	.createdAt {
 		margin-bottom: 1em;
 		font-size: 1rem;
 		font-weight: normal;
